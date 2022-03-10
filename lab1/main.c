7a8815f40cb7f0cab385fe3afeb716ca630a16da
soulrrrrr <chialun100@gmail.com>
2022-03-10 23:27:28 +0800

    fixed lab1 image

--- lab1/main.c
+++ lab1/main.c
@@ -1,95 +1,66 @@
-/*
- * Copyright (C) 2018 bzt (bztsrc@github)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
-
 #include "mbox.h"
 #include "reboot.h"
 #include "uart.h"
 
-/**
- * @brief compare string
- *
- * @param a first string
- * @param b second string
- */
-int strcmp(char *a, char *b) {
-    while (*a != '\0' && *a == *b) {
-        a++;
-        b++;
+int strcmp(char *s1, char *s2) {
+    while (*s1 != '\0' && *s1 == *s2) {
+        s1++;
+        s2++;
     }
-    return (*(unsigned char *)a) - (*(unsigned char *)b);
+    return (*(unsigned char *)s1) - (*(unsigned char *)s2);
 }
 
 void main() {
-    // set up serial console
     uart_init();
     uart_puts("Hello from Raspberry pi!\n");
 
-    char input[1024];
-    int len;
-
     while (1) {
-        len = 0;
-        uart_puts("\r# ");
+        uart_send('\r');
+        uart_send('#');
+        uart_send(' ');
+        int i = 0;
+        char input[1024];
+        char temp;
         while (1) {
-            input[len] = uart_getc();
-            if (input[len] == '\n') {
-                input[len] = '\0';
+            temp = uart_getc();
+            if (temp == '\n') {
                 uart_puts("\n");
+                input[i] = '\0';
                 break;
-            }
-            uart_send(input[len++]);
-            if (len >= 1023) break; // char array size
+            } else
+                uart_send(temp);
+
+            input[i] = temp;
+            i++;
         }
         if (strcmp(input, "help") == 0) {
-            uart_puts("help\t: print this help menu\n");
+            uart_puts("help\t: print this help memu\n");
             uart_puts("hello\t: print Hello World!\n");
-            uart_puts("mailbox\t: show board revision and ARM memory size\n");
+            uart_puts("mailbox\t: print mailbox information\n");
             uart_puts("reboot\t: reboot the device\n");
-        } else if (strcmp(input, "hello") == 0) {
+        } else if (strcmp(input, "hello") == 0)
             uart_puts("Hello World!\n");
-        } else if (strcmp(input, "mailbox") == 0) {
-            if (!get_board_revision()) {
-                uart_puts("Fail to get board revision!\n");
-                continue;
-            }
-            uart_puts("Board revision: ");
-            uart_hex(mbox[5]);
-            uart_puts("\n");
-            if (!get_arm_memory()) {
-                uart_puts("Fail to get ARM memory!\n");
-                continue;
-            }
-            uart_puts("Base address: 0x");
-            uart_hex(mbox[5]);
-            uart_puts("\n");
-            uart_puts("ARM memory size: ");
-            uart_uint(mbox[6]);
-            uart_puts(" bytes\n");
+        else if (strcmp(input, "mailbox") == 0) {
+            if (get_board_revision()) {
+                uart_puts("My board revision is : ");
+                uart_hex(mbox[5]);
+                uart_puts("\n");
+            } else
+                uart_puts("Failed to get board revision.\n");
+
+            if (get_arm_memory()) {
+                uart_puts("My ARM memory base address is : ");
+                uart_hex(mbox[5]);
+                uart_puts("\n");
+                uart_puts("My ARM memory size is : ");
+                uart_hex(mbox[6]);
+                uart_puts("\n");
+            } else
+                uart_puts("Failed to get arm memory.\n");
         } else if (strcmp(input, "reboot") == 0) {
-            uart_puts("rebooting...\n");
-            reset(10); // tick = 10
-        }
+            uart_puts("\n");
+            reset(10);
+        } else
+            uart_puts("Error input!\n");
     }
 }
